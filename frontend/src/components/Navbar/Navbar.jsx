72df40f7b13226af0f676689f970cdc1a6297efe
Rujula <rujula.malhotra@gmail.com>
2025-04-08 01:07:08 +0530

    rective page

--- frontend/src/components/Navbar/Navbar.jsx
+++ frontend/src/components/Navbar/Navbar.jsx
@@ -1,24 +1,25 @@
 import React, { useState } from 'react'
 import './Navbar.css'
 import { assets } from '../../assets/assets'
+import { Link } from 'react-router-dom';
 
 const Navbar = () => {
 
-    const [menu,setMenu] = useState("home");
+  const [menu, setMenu] = useState("home");
 
   return (
     <div className='navbar'>
-      {/*<img src={assets.logo} alt="" className="logo"/>*/}
+      <img src={assets.logo} alt="" className="logo" />
       <ul className='navbar-menu'>
-        <li onClick={()=>setMenu("home")} className={menu==="home"?"active":""}>Home</li>
-        <li onClick={()=>setMenu("menu")} className={menu==="menu"?"active":""}>Menu</li>
-        <li onClick={()=>setMenu("contact-us")} className={menu==="contact-us"?"active":""}>Contact Us</li>
+        <Link to="/" onClick={()=>{setMenu("home"); window.scrollTo({ top: 0, behavior: "smooth" });}}className={menu === "home" ? "active" : ""}>Home</Link>
+        <a href='#explore-menu' onClick={() => setMenu("menu")} className={menu === "menu" ? "active" : ""}>Menu</a>
+        <a href='#footer' onClick={() => setMenu("contact-us")} className={menu === "contact-us" ? "active" : ""}>Contact Us</a>
       </ul>
       <div className="navbar-right">
-        <img src={assets.search_icon} alt=""/>
+        <img src={assets.search_icon} alt="" />
         <div className="navbar-search-icon">
-            <img src={assets.basket_icon} alt=""/>
-            <div className="dot"></div>
+          <img src={assets.basket_icon} alt="" />
+          <div className="dot"></div>
         </div>
         <button>Sign In</button>
       </div>
