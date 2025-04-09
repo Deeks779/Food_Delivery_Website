7ee2653c48cc6ad7aa1a4e9dee19273d34528249
utkarsha-a <utkarsha.aherrao@gmail.com>
2025-04-09 18:12:39 +0530

    Created the Cart Page

--- frontend/src/pages/PlaceOrder/PlaceOrder.jsx
+++ frontend/src/pages/PlaceOrder/PlaceOrder.jsx
@@ -3,9 +3,16 @@ import './PlaceOrder.css'
 
 const PlaceOrder = () => {
   return (
-    <div>
+
+    <form className= 'place-order'>
+      <div className= "place-order-left">
+
+      </div>
+      <div className='place-order-right'>
+
+      </div>
       
-    </div>
+    </form>
   )
 }
 
