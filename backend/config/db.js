8f0fba1f3128b169bf0f444b0fa97f3898f277e0
Rujula <rujula.malhotra@gmail.com>
2025-04-10 02:37:22 +0530

    foods database creation

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,5 +1,6 @@
 import mongoose from "mongoose";
+import 'dotenv/config';
 
 export const connectDB = async () => {
-    await mongoose.connect().then(()=>console.log("DB connected!!"));
+    await mongoose.connect(process.env.MONGO_URI).then(()=>console.log("DB connected!!"));
 }
